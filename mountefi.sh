5db17788894f3fdb6c5d0432dbfdcd4eba500c49
elsorino <elso@protonmail.com>
2019-07-03 01:19:02 -0400

    Update mountefi.sh

--- mountefi.sh
+++ mountefi.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
+#This is only useful if you have a hackintosh
 sudo mkdir /Volumes/ESP
-sudo mount -t msdos /dev/disk0s1 /Volumes/ESP
+sudo mount -t msdos /dev/disk1s1 /Volumes/ESP
