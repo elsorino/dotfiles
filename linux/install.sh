19d3ac7065a794b2f27aa389f14d8003299e854e
elsorino <elso@protonmail.com>
2019-10-08 17:32:04 -0400

    Updated linux config

---
+++ linux/install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+mkdir -p ~/.config/{termite,polybar,i3,ranger}
+mkdir ~/.zsh 
+ln -s $PWD/termiteconfig ~/.config/termite/config
+ln -s $PWD/polybarconfig ~/.config/polybar/config
+ln -s $PWD/launch.sh ~/.config/polybar/launch.sh
+ln -s $PWD/i3config ~/.config/i3/config
+ln -s $PWD/ranger.conf ~/.config/ranger/rc.conf
+ln -s $PWD/Xresources ~/.Xresources
+ln -s $PWD/xinit ~/.xinitrc
+ln -s $PWD/zshrc ~/.zshrc
+git clone https://github.com/sindresorhus/pure ~/.zsh
