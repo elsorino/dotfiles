d7ad4490a4a630f501b0b1d34c68cfa1079d74a5
elsorino <elso@pm.me>
2020-06-22 17:34:40 -0400

    Added new blur config I use

--- linux/install.sh
+++ linux/install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-mkdir -p ~/.config/{termite,polybar,i3,ranger}
-mkdir ~/.zsh 
+mkdir -p ~/.config/{termite,polybar,i3,rofi,dunst,picom,micro}
 ln -s $PWD/termiteconfig ~/.config/termite/config
 ln -s $PWD/polybarconfig ~/.config/polybar/config
 ln -s $PWD/launch.sh ~/.config/polybar/launch.sh
 ln -s $PWD/i3config ~/.config/i3/config
-ln -s $PWD/ranger.conf ~/.config/ranger/rc.conf
-ln -s $PWD/Xresources ~/.Xresources
-ln -s $PWD/xinit ~/.xinitrc
+ln -s $PWD/rofi.rasi ~/.config/rofi/config.rasi
+ln -s $PWD/picom.conf ~/.config/picom/picom.conf
+ln -s $PWD/dunstrc ~/.config/dunst/dunstrc
+ln -s $PWD/microsettings.json ~/.config/micro/settings.json
 ln -s $PWD/bashrc ~/.bashrc
