f5c2e6472d19b2fef749d2e58b66eb2ab8ce7b5f
elsorino <elso@pm.me>
2020-07-02 20:14:25 -0400

    Updated tmux config

--- mac/mountefi.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-#This is only useful if you have a hackintosh
-sudo mkdir /Volumes/ESP
-sudo mount -t msdos /dev/disk0s1 /Volumes/ESP
--- zsh/installzsh.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p "$HOME/.zsh"
-git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
-fpath+=("$HOME/.zsh/pure")
-ln -s $PWD/zshrc ~/.zshrc
